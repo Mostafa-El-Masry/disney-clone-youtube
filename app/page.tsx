e8624be0641687400a7dc8a2f21cd8ec940ba798
Mostafa-El-Masry <mostafa.abdelfattah2021@gmail.com>
2024-10-14 20:12:30 +0300

    updating page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,28 @@
+"use client";
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  let pornStars = ["Mandy Muse", "Baliey Brooke", "Sophee Dee"];
+
+  const getMessage = () => {
+    return pornStars.length === 0 && (pornStars = ["Go Pray Dude"]);
+  };
+
+  return (
+    <div>
+      Home
+      {getMessage()}
+      {pornStars.map((pornstar) => (
+        <li
+          className="bg-orange-500 p-2 w-1/2 m-4"
+          key={pornstar}
+          onClick={() => console.log(pornstar)}
+        >
+          {pornstar}
+        </li>
+      ))}
+    </div>
+  );
 };
 
 export default Home;
